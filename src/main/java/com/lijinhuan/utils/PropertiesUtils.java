668352890fa74f800a46f843dee9bb5b7a05d5d4
Administrator <Administrator@win-10>
2017-03-08 21:58:44 +0800

    qwe

---
+++ src/main/java/com/lijinhuan/utils/PropertiesUtils.java
@@ -0,0 +1,40 @@
+package com.lijinhuan.utils;
+/**
+ * properties工具类
+ * @author Administrator
+ *
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesUtils{
+	
+	private static Properties PROPERTIES = new Properties();
+	
+	//初始化数据
+	static{
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream("redis.properties");
+			PROPERTIES.load(fileInputStream);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				fileInputStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static Object get(String key){
+		return PROPERTIES.get(key);
+	}
+}
+
---
+++ src/main/java/com/lijinhuan/utils/RedisUtils.java
@@ -0,0 +1,13 @@
+package com.lijinhuan.utils;
+/**
+ * redis π§æﬂ¿‡
+ * @author Administrator
+ *
+ */
+public class RedisUtils {
+	
+	
+	static{
+		
+	}
+}
